68755976c5127610cf4e55a688523f0e5638abe8
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-11-15 22:53:39 -0300

    Commit 8 dos arquivos

--- src/pages/VeiculoLista/index.tsx
+++ src/pages/VeiculoLista/index.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import { Row, Col } from "reactstrap";
 import { ContainerApp } from "../../components/Container";
 
+interface DataTypes {
+  nome: string;
+  pais_origem: string;
+  tipo: string;
+}
+
 export function VeiculoLista() {
-  
+  const [data, setData] = useState<DataTypes[]>([]);
+
   return (
     <ContainerApp>
       <h1>Lista de veiculos</h1>
+      <Row>
+        <Col md={12}></Col>
+      </Row>
     </ContainerApp>
   );
 }
